c065d85b11e49a615b7b23b5c62b702a0708fdb8
Julien <julienhalde9@gmail.com>
2024-01-24 21:15:57 -0500

    Started day 1. Need finish TODO

---
+++ day1.py
@@ -0,0 +1,14 @@
+import re
+
+total: int = 0
+f = open("d1-input.txt", "r")
+
+# Loop through each line in the file
+for line in f:
+    # Finds all the numbers and outputs list of ints with the all nums in the line
+    # todo because all the nums are one after the other it fucks up and takes all the nums
+    list_nums_in_current_line = list(map(int, re.findall("\d+", line)))
+    # Puts the two int nums together
+    line_num: int = int(f"{list_nums_in_current_line[0]}{list_nums_in_current_line[-1]}")
+    total += line_num
+print(total)
