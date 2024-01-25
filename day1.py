33cb5968fe11b9f0739a25c0ba21ecd3df2000cc
Julien <julienhalde9@gmail.com>
2024-01-25 09:33:55 -0500

    Finished Day1

--- day1.py
+++ day1.py
@@ -1,15 +1,16 @@
 import re
 
 total: int = 0
+list_nums: list[int] = []
 f = open("d1-input.txt", "r")
 
 # Loop through each line in the file
 for line in f:
+    list_nums = []
     # Finds all the numbers and outputs list of ints with the all nums in the line
-    # todo because all the nums are one after the other it fucks up and takes all the nums
-    list_nums_in_current_line = list(map(int, re.findall("\d+", line)))
-    # Puts the two int nums together
-    line_num: int = int(f"{list_nums_in_current_line[0]}{list_nums_in_current_line[-1]}")
-    total += line_num
+    for char in line:
+        if char.isdigit():
+            list_nums.append(int(char))
+    line_total: int = int(f"{list_nums[0]}{list_nums[-1]}")
+    total += line_total
 print(total)
-print("hello")
