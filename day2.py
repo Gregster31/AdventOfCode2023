ce339d3090425ddaa4d1cab5647590677ff87dd2
Julien <julienhalde9@gmail.com>
2024-02-07 22:56:37 -0500

    Started D2-P1

--- day2.py
+++ day2.py
@@ -1,25 +1,38 @@
-# Day 2
-f = open("d2-input.txt", "r")
-# 12 red cubes, 13 green cubes, and 14 blue cubes?
-MAX_RED: int = 12
-MAX_BLUE:int = 14
-MAX_GREEN:int = 13
+# open puzzle input: d2-input.txt
+def read_input(day_num):
+    with open(f'd{day_num}-input.txt', 'r') as f:
+        data = f.read().splitlines()
+    return data
 
-# Game 2: 6 red, 11 green; 4 blue, 4 green, 5 red; 11 green, 6 blue, 6 red
-for line in f:
-    # Separate Game from cubes
-    game_infos, cubes_infos = line.split(": ")
-    cubes_rolled: int | str = cubes_infos.split(", |; ")
-    num, color = cubes_rolled.split(" ")
-    # Add the nums of cubes
-    for x in len(cubes_rolled):
+### PART 1 ###
 
+# define a dictionary with the max cubes for each color
+max_cubes = {
+    'b': 14,
+    'r': 12,
+    'g': 13
+}
+def part1():
+    sum_of_games: int = 0
+    game_infos: str
+    cubes_infos: str
+    for i in read_input("2"):
+        grabs: list[str] = []
+        # Split1, splits the game num and cubes infos [Game 1] [3 red, 1 green; 2 blue, 1 red]
+        game_infos, cubes_infos = i.split(": ")
+        # Split2, splits the grabs [3 red, 1 green] [2 blue, 1 red]
+        grabs = cubes_infos.split("; ")
 
-
-
-
-
-
-
-
-
+        # Split3, splits the cube infos [3 red] [1 green]
+        for x in grabs:
+            different_colors: list[str] = x.split(", ")
+            # Check for color, then check if num of cubes is under limit
+            for y in different_colors:
+                if y[2] in max_cubes:
+                    # Check if num of cubes is lower than limit
+                    if y[0] < max_cubes[y[2]]:
+                        continue
+                    else:
+                        print("This game does not exist. Too many cubes grabbed!")
+                else:
+                    print("Color does not exist!")
